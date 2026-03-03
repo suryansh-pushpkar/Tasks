358c1477c5afe8e7d1bdd708832aca3e02114a04
unknown <suryansh.pushpkar@webkorps.com>
2026-03-04 03:58:32 +0530

    aop practice

--- Practice/demo/src/main/java/com/info/aspect/LogAspect.java
+++ Practice/demo/src/main/java/com/info/aspect/LogAspect.java
@@ -1,10 +1,8 @@
 package com.info.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
