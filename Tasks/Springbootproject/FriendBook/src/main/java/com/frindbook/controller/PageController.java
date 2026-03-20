0b90e64168774efd1048e5beb2e7ca45775f5bfa
unknown <suryansh.pushpkar@webkorps.com>
2026-03-21 00:30:23 +0530

    Pages and routings

---
+++ Tasks/Springbootproject/FriendBook/src/main/java/com/frindbook/controller/PageController.java
@@ -0,0 +1,28 @@
+package com.frindbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/signup")
+    public String showSignupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+    
+    @GetMapping("/about")
+    public String showAboutPage() {
+        return "about";
+    }
+
+    @GetMapping("/")
+    public String showIndexPage() {
+        return "index";
+    }
+}
