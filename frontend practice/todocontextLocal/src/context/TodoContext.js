e33dbad767ada22f951d3126f2bc300a14f4bc15
surynash-pushpkar <suryansh.pushpkar@webkorps.com>
2026-04-15 02:44:51 +0530

    Todo

--- frontend practice/todocontextLocal/src/context/TodoContext.js
+++ frontend practice/todocontextLocal/src/context/TodoContext.js
@@ -1,8 +1,14 @@
 import { createContext, useContext } from "react";
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+  todos: [{ id: 1, todo: "Todo Message", completed: false }],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 
+export const useTodoContext = () => {
+  return useContext(TodoContext);
+};
 
-export const useTodoContext= () =>{
-    return useContext(TodoContext);
-}
-
+export const TodoProvider = TodoContext.Provider;
